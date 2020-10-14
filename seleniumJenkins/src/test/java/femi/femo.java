54ef7eb51c1d6f380913fcb8effff700394b70fa
hp <hp@DESKTOP-V9I2HOB>
2020-10-14 02:12:05 +0100

    seleniumJenkins in github

---
+++ seleniumJenkins/src/test/java/femi/femo.java
@@ -0,0 +1,15 @@
+package femi;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class femo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver86\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.ebay.com/");
+     
+	}
+
+}
