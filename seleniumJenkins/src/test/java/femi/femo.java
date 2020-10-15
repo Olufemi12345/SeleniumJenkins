e9f4154a7545a93eff6bfe6eab7b163739b4d719
hp <hp@DESKTOP-V9I2HOB>
2020-10-15 01:32:18 +0100

    Improvement

--- seleniumJenkins/src/test/java/femi/femo.java
+++ seleniumJenkins/src/test/java/femi/femo.java
@@ -7,9 +7,9 @@ public class femo {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver86\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.ebay.com/");
-     
+
 	}
 
 }
